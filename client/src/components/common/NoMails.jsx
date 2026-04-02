8f3e126bfc82446eeb06069fdd70154bd026c527
Maria Rodriguez <mrodr.contact@gmail.com>
2026-04-02 15:03:42 -0400

    UI: left-align helper text + lighter typography

--- client/src/components/common/NoMails.jsx
+++ client/src/components/common/NoMails.jsx
@@ -7,11 +7,18 @@ const Component = styled(Box)({
     width: '100%',
     alignItems: 'center',
     marginTop: 8,
-    opacity: .8,
+    opacity: 0.82
+});
+
+const Content = styled(Box)({
+    width: '100%',
+    maxWidth: 520,
+    textAlign: 'left'
 });
 
 const StyledDivider = styled(Divider)({
     width: '100%',
+    maxWidth: 520,
     marginTop: 26,
     marginBottom: 10
 })
@@ -19,10 +26,14 @@ const StyledDivider = styled(Divider)({
 const NoMails = ({ message }) => {
     return (
         <Component>
-            <Typography sx={{ fontWeight: 600 }}>{message.heading}</Typography>
-            <Typography sx={{ maxWidth: 520, textAlign: 'center', whiteSpace: 'pre-line' }}>
-                {message.subHeading}
-            </Typography>
+            <Content>
+                <Typography sx={{ fontWeight: 400, color: '#3c4043' }}>
+                    {message.heading}
+                </Typography>
+                <Typography sx={{ fontWeight: 300, color: '#5f6368', whiteSpace: 'pre-line', marginTop: 0.5 }}>
+                    {message.subHeading}
+                </Typography>
+            </Content>
             <StyledDivider />
         </Component>
     )
